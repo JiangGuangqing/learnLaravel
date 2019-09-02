335ecad7b86f754b25368b8e0ed2e2680341a740
JiangGuangqing <hx@aumc.cc>
2019-09-02 09:43:40 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,16 @@
 <!doctype html>
+<html>
 <head>
     <meta charset="UTF-8">
     <title>@yield('title','Sample') - Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
